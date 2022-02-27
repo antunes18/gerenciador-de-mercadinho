52bf8a2fcb1bab22400e2c9e4f67b92ced206e61
Thiago Antunes dos Santos <thiagoantunes0101@gmail.com>
2022-02-27 13:56:42 -0300

    Setup pool and database connection

--- app.js
+++ app.js
@@ -22,6 +22,22 @@ app.get('', (req, res) => {
   res.render('home');
 });
 
+// Pool Setup
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+// Database connection
+pool.getConnection((error, conn) => {
+  if (error) throw error; // not connected
+
+  console.log(`Connected as ID ${conn.threadId}`);
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
